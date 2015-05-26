a2b6d0eef2015b7498571cc7114f04c26b54c957
Squall949 <webber949@gmail.com>
2015-05-26 16:56:26 +0800

    delete

--- CordovaAppLinkCaller/www/AppLinkCaller.js
+++
@@ -1,7 +0,0 @@
-function AppLinkCaller() { };
-
-AppLinkCaller.openLink = function (url, onSuccess) {
-    cordova.exec(onSuccess, null, 'AppLinkCaller', 'OpenLink', [url]);
-};
-
-module.exports = AppLinkCaller;
